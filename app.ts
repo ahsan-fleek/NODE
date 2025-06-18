c068c1ae58b6784fc17b00161c9a8aa5398a7687
Ahsan Shaikh <ahsan.shaikh@fleekbiz.com>
2025-06-19 00:00:14 +0500

    Completed Todo

--- app.ts
+++ app.ts
@@ -4,12 +4,17 @@ import todoRoutes from './routes/todo';
 import systemRoutes from './routes/system';
 import { defaultRoutes } from './controllers/global/default';
 import ErrorHandler from './utils/helpers/errorHandler';
+import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser())
 
 export const registerRoutes = () => {
   app.use('/api/system/', systemRoutes);
--- build/app.js
+++ build/app.js
@@ -10,10 +10,15 @@ const todo_1 = __importDefault(require("./routes/todo"));
 const system_1 = __importDefault(require("./routes/system"));
 const default_1 = require("./controllers/global/default");
 const errorHandler_1 = __importDefault(require("./utils/helpers/errorHandler"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(express_1.default.json({ limit: "16kb" }));
+app.use(express_1.default.urlencoded({ extended: true, limit: "16kb" }));
+app.use((0, cookie_parser_1.default)());
 const registerRoutes = () => {
     app.use('/api/system/', system_1.default);
     app.use('/api/todo/', todo_1.default);
